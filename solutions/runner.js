94ac794885540c9120ea6ec1af3fba98e6bed10f
Sophie Kim <86454825+sophkim@users.noreply.github.com>
2022-03-17 11:24:55 +0900

    Create runner.js

---
+++ solutions/runner.js
@@ -0,0 +1,16 @@
+// 해시
+
+function solution(participant, completion) {
+    var answer = '';
+    
+    participant.sort();
+    completion.sort();
+    
+    for (let i = 0; i < participant.length; i++) {
+        if (participant[i] !== completion[i]) {
+        answer = participant[i];
+             return answer;
+        }
+    }
+}
+
