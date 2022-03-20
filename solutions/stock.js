c851317e7a3aa1e5bf3feef4e01d54a57bfcc052
Sophie Kim <86454825+sophkim@users.noreply.github.com>
2022-03-20 22:00:27 +0900

    Update stock.js

--- solutions/stock.js
+++ solutions/stock.js
@@ -1,7 +1,8 @@
-// 스택 큐
+// 스택 큐: 주식가격
 // 이중 for문 사용
 
 function solution(prices) {
+    // 가격 개수만큼의 배열을 만들어 0으로 채움
     let answer = new Array(prices.length).fill(0);
     
     for (let i = 0, len = prices.length - 1; i < len; i++) {
