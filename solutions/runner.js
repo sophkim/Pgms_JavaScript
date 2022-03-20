08839ccf95f932a3349030ff83c304ca20ea38ab
Sophie Kim <86454825+sophkim@users.noreply.github.com>
2022-03-20 22:01:41 +0900

    Update runner.js

--- solutions/runner.js
+++ solutions/runner.js
@@ -1,12 +1,14 @@
-// 해시
+// 해시: 완주하지 못한 선수
 
 function solution(participant, completion) {
     var answer = '';
     
+    // 오름차순 정렬
     participant.sort();
     completion.sort();
     
     for (let i = 0; i < participant.length; i++) {
+        // 참가자 중 완료자에 없는 선수들
         if (participant[i] !== completion[i]) {
         answer = participant[i];
              return answer;
