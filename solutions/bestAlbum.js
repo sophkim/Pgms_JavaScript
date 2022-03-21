f4adf0b1217b0d68e42b832daabab5fbb2e0ccb5
Sophie Kim <86454825+sophkim@users.noreply.github.com>
2022-03-21 11:35:25 +0900

    Update bestAlbum.js

--- solutions/bestAlbum.js
+++ solutions/bestAlbum.js
@@ -1,2 +1,48 @@
 // 해시
 // Programmers - Hash: BestAlbum
+
+function solution(genres, plays) {
+    // 인덱스 별 장르, 횟수 저장
+    let songs = 
+        genres.map((genre,index) => {
+            return {
+                no: index,
+                genre: genre,
+                playCount: plays[index]
+            };
+        });
+    
+    // 장르 별 횟수를 저장할 배열 선언
+    let genrePlayCount = [];
+    
+    // songs 배열을 돌며
+    songs.forEach(song => {
+        let thisGenre = genrePlayCount.find(genrePlay => genrePlay.genre === song.genre);
+        
+        // genrePlayCount에 저장되어 있지 않은 장르라면
+        if(!thisGenre) {
+            // 장르와 횟수 저장
+            genrePlayCount.push({
+                genre: song.genre, totalPlayCount: song.playCount
+            });
+        } else {
+            // 저장되어 있다면 횟수만 누적
+            thisGenre.totalPlayCount += song.playCount;
+        }
+    });
+    
+    genrePlayCount.sort((a, b) => b.totalPlayCount - a.totalPlayCount);
+    
+    let answer = [];
+    genrePlayCount.forEach(genrePlay => {
+        let thisGenreSongs = songs.filter(song => genrePlay.genre === song.genre);
+        thisGenreSongs.sort((a, b) => b.playCount - a.playCount);
+        answer.push(thisGenreSongs[0].no);
+        if(thisGenreSongs.length > 1) {
+            answer.push(thisGenreSongs[1].no);
+        }
+    });
+
+return answer;
+    
+}
