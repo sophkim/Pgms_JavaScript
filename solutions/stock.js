50fe64d2a5d8e36c91d6b54b85446beb7e084759
Sophie Kim <86454825+sophkim@users.noreply.github.com>
2022-04-03 17:04:20 +0900

    Update stock.js

--- solutions/stock.js
+++ solutions/stock.js
@@ -5,8 +5,10 @@ function solution(prices) {
     // 가격 개수만큼의 배열을 만들어 0으로 채움
     let answer = new Array(prices.length).fill(0);
     
+    // 배열을 돌며
     for (let i = 0, len = prices.length - 1; i < len; i++) {
         answer[i] = 1;
+        // 다음 요소(가격)보다 작다면
         if (prices[i] <= prices[i + 1]) {
             for (let j = i + 1; j < len; j++) {
                 if (prices[i] > prices[j]) break
